1be6ee9ce78e1ddd08ee6ecbd82b4f230512aef3
GyUliAmArTinsPiquEira <gyulia.piqueira@sptech.school>
2023-05-29 18:51:25 -0300

    dash

--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -1,54 +1,33 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idResultado) {
+function buscarUltimasMedidas(idUsuario, limite_linhas) {
 
     instrucaoSql = ''
 
-    if (process.env.AMBIENTE_PROCESSO == "producao") {
         instrucaoSql = `select Acertos, 
                     Erros
                     from Resultado
-                    where fkUsuario = ${idResultado}`;
-    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select Acertos, 
-                    Erros
-                    from Resultado
-                    where fkUsuario = ${idResultado}`;
-    } else {
-        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
-        return
-    }
+                    where fkUsuario = ${idUsuario}
+                    order by idResultado desc limit ${limite_linhas}`
+                    ;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function buscarMedidasEmTempoReal(idResultado) {
+function buscarMedidasEmTempoReal(idUsuario) {
 
     instrucaoSql = ''
 
-    if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select top 1
-        Acertos, 
-        Erros
-                        CONVERT(varchar, momento, 108) as momento_grafico, 
-                        fkQuiz 
-                        from Resultado where fkQuiz = ${1} 
-                    order by id desc`;
+    instrucaoSql = `select Acertos, 
+                Erros
+                from Resultado
+                where fkUsuario = ${idUsuario}
+                order by idResultado desc limit 1
+                `;
 
-    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select 
-        Acertos, 
-        Erros,
-        fkQuiz
-                        from Resultado where fkQuiz = ${1} `;
-    } else {
-        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
-        return
-    }
-
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+console.log("Executando a instrução SQL: \n" + instrucaoSql);
+return database.executar(instrucaoSql);
 }
 
 
